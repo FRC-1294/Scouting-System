3a100bef3934a9f6f5896403202e2270c17cccc9
Sam Tischaefer <samtisch@live.com>
2021-11-18 09:27:00 -0800

    Apply formatting

--- README.md
+++ README.md
@@ -4,12 +4,15 @@ This is a scouting system for the FRC 2022 game, Rapid React!
 
 ## Tech
 
-- Svelte.JS
-- Node with ExpressJS
+-   Svelte.JS
+-   Node with ExpressJS
 
 ## How to
+
 ### Develop
+
 You need NodeJS V16 with NPM
+
 1. Clone repo
 2. Open terminal and run `npm i`
 3. Run `npm run dev` to start development server
--- server.js
+++ server.js
@@ -6,15 +6,13 @@ const path = require('path')
 
 //Database configs
 
-
 //Routes for database
-
 
 //Svelte and express stuff (NO TOUCHY)
 app.use(express.static('public'))
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 app.listen(port, () => {
-   console.log(`Server is up at port ${port}`)
+	console.log(`Server is up at port ${port}`)
 })
