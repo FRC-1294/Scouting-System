31f73a32a68fb500d5c632db10e9c837d7296bf6
Sam Tischaefer <samtisch@live.com>
2021-11-21 11:12:52 -0800

    Separate servers by port and merge into one file

--- web.js
+++ web.js
@@ -1,22 +1,54 @@
 var express = require('express');
-var app = express();
-let path = require('path')
-let fs = require('fs')
-let port = 3000 || process.env.PORT
+var webApp = express();
+let portWeb = 3000
+let portSocket = 4000
 
+//
+//WEB
+//
 //Static directory
-app.use(express.static("public"))
+webApp.use(express.static("public"))
 
 //Logging middleware
-app.use((req, res, next) => {
-    console.log(req.url)
+webApp.use((req, res, next) => {
+    console.log("Request: " + req.url)
     next();
 })
 
 //Let Svelte handle all requests
-app.get('*', (req, res) => {
+webApp.get('*', (req, res) => {
     res.sendFile('public/main.html', {root: __dirname })
 })
 
-//Listen app
-app.listen(port, () => {console.log(`Server listening on port ${port}`)})
+//
+//SOCKET
+//
+//Setup server
+var socketServer = require('http').createServer();
+var io = require('socket.io')(socketServer,{
+  cors: {
+    origin: "*",
+    methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders:["ThisIsASuperSecretSecureStringForUseToAuthentimicateTheRandomnessOnMyBackendYouSeriouslyShouldNotUseThisHNxQ3VEvbESFVES32423513452BSIUFVHSFD"],
+    credentials: false
+  }
+});
+
+//Events
+io.on('connection', function(client) {
+  console.log('Client connected...');
+  
+  client.on('join', function(data) {
+    console.log(data);
+      client.emit('messages', 'Hello from server');
+
+    setTimeout(() => {
+        client.emit("serverMessage", "AA")
+    }, 2000)
+  });
+});
+
+
+//Listen apps
+webApp.listen(portWeb, () => {console.log(`Server listening on port ${portWeb}`)})
+socketServer.listen(portSocket, () => console.log(`Socket io listening on port ${portSocket}`));
