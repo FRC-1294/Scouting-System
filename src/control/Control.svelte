5040d02e4dd323b5632f9189fa441703a057640b
Sam Tischaefer <samtisch@live.com>
2021-11-29 10:54:20 -0800

    Dynamic scouts and boxes

--- src/control/Control.svelte
+++ src/control/Control.svelte
@@ -15,21 +15,34 @@
 	function leClicc() {
 		socket.emit("setupMatch", matchNumberToSetUp)
 	}
+	let scouttt = ["A", "B"]
+	function leClicc2() {
+		scouttt.push("aaa")
+		scouttt = scouttt
+	}
+
+	//Kick a scout off
+	function kickOff(id) {
+		alert(id)
+	}
 </script>
 
 <main>
 <style>	
 	body {
-		background-color: #98ff83;
+		background-color: #FFFFFF;
 	}
 </style>
 
 	<h1>ADMIN</h1>
 	<input bind:value={matchNumberToSetUp} />
 	<button on:click={leClicc}>Clicc</button>
-	<br>
-	<ScoutWidget name="h" id="1234" robot="1294" status="scouting" />
-	<ScoutWidget name="b" id="8888" robot="2941" status="disconnected" />
-	<br>
+	<button on:click={leClicc2}>AAAAAAAAAAAA</button>	
 	<button on:click={() => {socket.emit("endMatch")}}>End Match</button>
+	<br>
+	<ScoutWidget name="h" id="1234" robot="1294" status="scouting" boot={kickOff}/>
+	<ScoutWidget name="b" id="8888" robot="2941" status="disconnected" boot={kickOff}/>
+	{#each scouttt as newScoutName, newScout}		
+		<ScoutWidget name={newScoutName} id={newScout} robot="123" status="disconnected" boot={kickOff}/>
+	{/each}
 </main>
--- src/control/ScoutWidget.svelte
+++ src/control/ScoutWidget.svelte
@@ -4,22 +4,31 @@
     export let robot
     //Status: disconnected, connected, scouting, submit
     export let status
+    export let boot
+    const defaultStyle = "width:23%; height:400px; margin: auto; float:left; border: 5px solid #000000; padding: 10px; background-color:"
+    let divStyle
+    if(status == "disconnected") {
+        divStyle = defaultStyle + "#ff9999"
+    }
+    if (status == "connected") {
+        divStyle = defaultStyle + "#99c2ff"
+    }
+    if (status == "scouting") {
+        divStyle = defaultStyle + "#80ff80"
+    }
+    if (status == "submit") {
+        divStyle = defaultStyle + "#ff66ff"
+    }
 </script>
 
 <main>
-    <div>
+    <div class="a" style={divStyle}>
         <h1>{name}</h1>
         <h2>{id}</h2>
         <h2>R{robot}</h2>
         <h2>{status}</h2>
         {#if status == "disconnected"}
-        <style>div:{background-color: #ff9999;}</style>
-        {:else if status == "connected"}
-        <style>div:{background-color: #99c2ff;}</style>
-        {:else if status == "scouting"}
-        <style>div:{background-color: #80ff80;}</style>
-        {:else if status == "submit"}
-        <style>div:{background-color: #ff66ff;}</style>
+            <button on:click={() => {boot(id)}}>BOOT</button>
         {/if}
     </div>    
 </main>
