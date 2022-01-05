78be3fc7466028c5aee28bdc644bfbeebee3ce56
Sam Tischaefer <samtisch@live.com>
2022-01-04 20:11:45 -0800

    start scoutmanager

--- src/backend/scouts.ts
+++ src/backend/scouts.ts
@@ -9,11 +9,23 @@ export class Scout {
 	status: 'connected' | 'disconnected' | 'scouting' | 'submit'
 	isScouting: Boolean
 	robotScouting: Number
+	isDiscord: Boolean
+	discordId: Number //-1 if none
 	/**
 	 * Pings this scout, or shows an alert on the control screen
 	 */
 	public ping() {
-
+		if(this.isDiscord) {
+			if(this.discordId > 0) {
+				//TODO ping admin
+			} else {
+				//Only way to get here is if isDiscord is true and discordId is <= 0
+				//TODO throw error
+			}
+		} else {
+			//TODO discord ping
+		}
+		
     }
 }
 
