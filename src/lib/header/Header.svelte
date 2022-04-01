cdb373f7584f3e493580cd3658ede667e88b8d94
Sam Tischaefer <samtisch@live.com>
2022-04-01 10:37:03 -0700

    Aesthetic

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -5,7 +5,7 @@
 
 <header>
 	<div class="corner">
-		<img src="/favicon.png" alt="SvelteKit" />
+		<img src="/favicon.png" alt="The Scouting System!" />
 	</div>
 
 	<nav>
@@ -20,11 +20,11 @@
 			<!--<li class:active={$page.url.pathname.includes("/data")}>
 				<a sveltekit:prefetch href="/data">Data</a>
 			</li>
-							
+					
+			-->		
 			<li class:active={$page.url.pathname.includes("/pit")}>
 				<a sveltekit:prefetch href="/pit/list">Pits</a>
 			</li>
-			-->
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
