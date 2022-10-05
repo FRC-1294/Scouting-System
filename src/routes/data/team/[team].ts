c4c749b828a2b42325cf62d57564d848cb161a9d
Sam Tischaefer <samtisch@live.com>
2022-10-05 16:58:31 -0700

    Fixed number

--- src/routes/data/team/[team].ts
+++ src/routes/data/team/[team].ts
@@ -1,13 +1,14 @@
 import { getListOfAllTeams, getPitTeamData, getTeamData, getTeamNotes } from '$lib/db';
 
 export let get = async function ({params}) {
-	let aggData = await getTeamData(params.team)
+	let teamNumber = Number(params.team)
+	let aggData = await getTeamData(teamNumber)
 			
-			let pitData = await getPitTeamData(params.team)
+	let pitData = await getPitTeamData(teamNumber)
 			
-			let notes = await getTeamNotes(params.team)
+	let notes = await getTeamNotes(teamNumber)
 
-	let finishedData: App.Team = {teamNumber: params.team, notes: notes, matchData: aggData, pitData: pitData}
+	let finishedData: App.Team = {teamNumber: teamNumber, notes: notes, matchData: aggData, pitData: pitData}
 	return {
 		body: {
 			data: finishedData
