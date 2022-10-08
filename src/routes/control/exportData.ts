aebad26185d6c3b8a725d29b6e7021af4251ed1d
Sam Tischaefer <samtisch@live.com>
2022-10-08 00:22:52 -0700

    Export notes too

--- src/routes/control/exportData.ts
+++ src/routes/control/exportData.ts
@@ -1,12 +1,11 @@
 
-import { aggregate, importEventMatchData } from '$lib/db';
+import { aggregate, getTeamNotes, importEventMatchData } from '$lib/db';
 import fs from "fs"
 import path from "path"
 import type { RequestHandler } from '@sveltejs/kit';
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export let get: RequestHandler = async function () {
     console.log("Exporting CSV")
-    let res: App.AggregatedTeamData[];
     let csvName = `${Math.round(new Date().getTime() / 1000)}.csv`
 
     //Get CSV Path
@@ -14,12 +13,15 @@ export let get: RequestHandler = async function () {
 
     console.log("H " + csvName)
     try {
+        let res: App.AggregatedTeamData[];
         res = await aggregate();
+        
         console.log(res)
-        fs.appendFileSync("./static/export/" + csvName, "Team Number, Average Cargo Auto, Average Cargo Teleop, Average Missed cargo, Climb low, Climb mid, Climb high, Climb traverse, Defense %, Lower hub, upper hub, reliable %, stuck %\r\n")
+        fs.appendFileSync("./static/export/" + csvName, "Team Number,Average Cargo Auto,Average Cargo Teleop,Average Missed cargo,Climb low,Climb mid,Climb high,Climb traverse,Defense %,Lower hub,upper hub,reliable %,stuck %,notes\r\n")
         // eslint-disable-next-line @typescript-eslint/no-empty-function
-        res.forEach((t: App.AggregatedTeamData) => {
-            let string = `${t._id},${t.averageCargoAuto},${t.averageCargoTele},${t.averageMissedCargo},${t.climb.low.percent},${t.climb.mid.percent},${t.climb.high.percent},${t.climb.traverse.percent},${t.defensePercent},${t.hub.lower.percent},${t.hub.upper.percent},${t.reliablePercent},${t.stuckPercent}\r\n`
+        res.forEach(async (t: App.AggregatedTeamData) => {
+            let notes = await getTeamNotes(t._id)
+            let string = `${t._id},${t.averageCargoAuto},${t.averageCargoTele},${t.averageMissedCargo},${t.climb.low.percent},${t.climb.mid.percent},${t.climb.high.percent},${t.climb.traverse.percent},${t.defensePercent},${t.hub.lower.percent},${t.hub.upper.percent},${t.reliablePercent},${t.stuckPercent},${notes.notes}\r\n`
             console.log(string)
             fs.appendFileSync("./static/export/" + csvName, string)
         });
