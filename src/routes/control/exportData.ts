9b55fcb0fb42abaa5ed7ff7e4173cc5037e14bf6
Sam Tischaefer <samtisch@live.com>
2022-10-08 23:53:40 -0700

    Fix race condition

--- src/routes/control/exportData.ts
+++ src/routes/control/exportData.ts
@@ -19,12 +19,15 @@ export let get: RequestHandler = async function () {
         console.log(res)
         fs.appendFileSync("./static/export/" + csvName, "Team Number,Average Cargo Auto,Average Cargo Teleop,Average Missed cargo,Climb low,Climb mid,Climb high,Climb traverse,Defense %,Lower hub,upper hub,reliable %,stuck %,notes\r\n")
         // eslint-disable-next-line @typescript-eslint/no-empty-function
-        res.forEach(async (t: App.AggregatedTeamData) => {
-            let notes = await getTeamNotes(t._id)
-            let string = `${t._id},${t.averageCargoAuto},${t.averageCargoTele},${t.averageMissedCargo},${t.climb.low.percent},${t.climb.mid.percent},${t.climb.high.percent},${t.climb.traverse.percent},${t.defensePercent},${t.hub.lower.percent},${t.hub.upper.percent},${t.reliablePercent},${t.stuckPercent},${notes.notes}\r\n`
-            console.log(string)
-            fs.appendFileSync("./static/export/" + csvName, string)
-        });
+        for (const aTeam in res) {
+            if (Object.prototype.hasOwnProperty.call(res, aTeam)) {
+                const t = res[aTeam];                
+                let notes = await getTeamNotes(t._id)
+                let string = `${t._id},${t.averageCargoAuto},${t.averageCargoTele},${t.averageMissedCargo},${t.climb.low.percent},${t.climb.mid.percent},${t.climb.high.percent},${t.climb.traverse.percent},${t.defensePercent},${t.hub.lower.percent},${t.hub.upper.percent},${t.reliablePercent},${t.stuckPercent},${notes.notes.replace(",", "  ")}\r\n`
+                console.log(string)
+                fs.appendFileSync("./static/export/" + csvName, string)
+            }
+        }
     } catch (error) {
         return {
             status: 500,
